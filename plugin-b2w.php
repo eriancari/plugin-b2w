bf9433e8172d1be92ed27a87205effaea47555cf
Erika Carriedo <ann@aplusanswers.com>
2025-08-13 14:42:31 +0800

    Initialize a simple plugin to create custom elementor widgets

---
+++ plugin-b2w.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Plugin for Bootstrap to Wordpress 2.0
+ * Description: Custom Elementor Widgets for our Bootstrap to WordPress Course
+ * Plugin URI:  https://bootstraptowordress.com/
+ * Version:     1.0.0
+ * Author:      Kei Carriedo
+ * Author URI:  https://eac.com/
+ * Text Domain: plugin-b2w
+ *
+ * Requires Plugins: elementor
+ * Elementor tested up to: 3.25.0
+ * Elementor Pro tested up to: 3.25.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+$plugin_imgs = plugin_dir_url( __FILE__ ) . 'assets/images';
